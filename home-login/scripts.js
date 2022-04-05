a2acac4b3fd82e07c6373940006cf09933628ff8
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2022-04-05 16:39:07 +0530

    URL update

--- home-login/scripts.js
+++ home-login/scripts.js
@@ -58,7 +58,7 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
-    $("#image").attr('src',profile.getImageurl());
+    $("#image").attr('src',profile.getImageUrl());
     $(".data").css("display","block");
     $(".g-signin2").css("display","none");
 
