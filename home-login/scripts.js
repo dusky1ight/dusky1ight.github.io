fa093521325978440c0fd41f2d2b1faba40f44a4
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2022-04-05 16:45:05 +0530

    comment uncommented

--- home-login/scripts.js
+++ home-login/scripts.js
@@ -62,8 +62,8 @@ function onSignIn(googleUser) {
     $(".data").css("display","block");
     $(".g-signin2").css("display","none");
 
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
     // console.log('Image URL: ' + profile.getImageUrl());
     // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
